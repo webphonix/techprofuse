1bf298295cfa70667f8a1297268273d2d9a9eea2
Guru <kumar@atlastech.dev>
2022-11-13 04:29:19 +0530

    tt

--- google_invoice.py
+++ google_invoice.py
@@ -9,7 +9,16 @@ import yaml
 file_name = 'secrets.yaml'
 directory = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(directory, file_name)
-conf = yaml.safe_load(Path(file_path).read_text())
+
+conf = {}
+with open(file_path, "r") as stream:
+    try:
+        conf = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+        conf = {}
+
+conf = yaml.safe_load(os.path.expanduser(file_path).read_text())
 
 db_username = conf['DB_USERNAME']
 db_password = conf['DB_PASSWORD']
@@ -21,11 +30,11 @@ db_engine = create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
                                  db_ip, db_name))
 
 
-# def get_data():
-#     # print("get_data called")
-#     sql = "SELECT * FROM GOOGLE_INVOICE;"
-#     df = pd.read_sql(sql, con=db_engine)
-#     return df.to_json(orient='records')
+def get_data():
+    # print("get_data called")
+    sql = "SELECT * FROM GOOGLE_INVOICE;"
+    df = pd.read_sql(sql, con=db_engine)
+    return df.to_json(orient='records')
 
 
 # pp = get_data()
