92d777d61bb13c8731cc6f13d99b6bcc13563129
Guru <kumar@atlastech.dev>
2022-11-13 01:33:33 +0530

    added login

--- NaagRoute.py
+++ NaagRoute.py
@@ -2,8 +2,6 @@ import actionhandlers as naag
 import tornado.web
 import google_invoice
 
-data = google_invoice.get_data
-# data = {'name': 'Guru'}
 
 NaagRoute = [
     (r'/node_modules/(.*)', tornado.web.StaticFileHandler,
@@ -14,5 +12,6 @@ NaagRoute = [
     (r"/", naag.JsonAction, {'data': google_invoice.get_data}),
     (r"/home", naag.FileAction, {'kargs': {'file': 'home.html'}}),
     (r"/index", naag.FileAction, {'kargs': {'file': 'index.html'}}),
-    (r"/login", naag.FileAction, {'kargs': {'file': 'login.html'}}),
+    (r"/login", naag.AuthAction, {'kargs': {'file': 'login.html'}}),
+    (r"/logout", naag.LogoutAction),
 ]
--- actionhandlers.py
+++ actionhandlers.py
@@ -5,10 +5,23 @@ import os
 
 
 class BaseAction(tornado.web.RequestHandler):
-    def get_current_user(self):
-        return self.get_secure_cookie("username")
+    def get_login_url(self):
+        return u"/login"
 
-# This
+    def get_current_user(self):
+        # return self.get_secure_cookie("username")
+        user_json = self.get_secure_cookie("user")
+        if user_json:
+            return tornado.escape.json_decode(user_json)
+        else:
+            return None
+            # self.redirect("/login")
+            # return
+        # if not self.current_user:
+        #     self.redirect("/login")
+        #     return
+
+
 class StaticFileAction(tornado.web.StaticFileHandler):
     def initialize(self, path):
         self.path = path
@@ -19,9 +32,45 @@ class FileAction(BaseAction):
         self.kargs = kargs
 
     def get(self):
+        if not self.current_user:
+            self.redirect("/login")
+            return
         print(self.kargs['file'])
         file = os.path.join("views", self.kargs['file'])
         self.render(file)
+
+
+class AuthAction(BaseAction):
+    def initialize(self, kargs):
+        self.kargs = kargs
+
+    def get(self):
+        print(self.kargs['file'])
+        file = os.path.join("views", self.kargs['file'])
+        self.render(file, next=self.get_argument("next", "/"))
+
+    def post(self):
+        email = self.get_argument("username", "")
+        password = self.get_argument("password", "")
+        if email == "gurudevk@techprofuse.com" and password == "admin123":
+            self.set_current_user(email)
+            self.redirect(self.get_argument("next", u"/"))
+        else:
+            error_msg = u"?error=" + \
+                tornado.escape.url_escape("Login incorrect.")
+            self.redirect(u"/login" + error_msg)
+
+    def set_current_user(self, user):
+        if user:
+            self.set_secure_cookie("user", tornado.escape.json_encode(user))
+        else:
+            self.clear_cookie("user")
+
+
+class LogoutAction(BaseAction):
+    def get(self):
+        self.clear_cookie("user")
+        self.redirect(self.get_argument('next', u'/login'))
 
 
 class JsonAction(BaseAction):
@@ -29,8 +78,12 @@ class JsonAction(BaseAction):
         self.data = data()
 
     def get(self):
+        if not self.current_user:
+            self.redirect("/login")
+            return
         # self.write(self.data)
         self.finish(self.data)
+
 
 # class AuthAction(BaseAction):
 #     # @NaagFilters.naagauth
@@ -46,12 +99,6 @@ class JsonAction(BaseAction):
 #             self.redirect("/dashboard")
 #         else:
 #             self.redirect("/login")
-
-
-# class LogoutAction(BaseAction):
-#     def get(self):
-#         self.clear_cookie('username')
-#         self.redirect(self.get_argument('next', '/dashboard'))
 
 
 # class HomeAction(BaseAction):
@@ -72,12 +119,3 @@ class JsonAction(BaseAction):
 #         r = self.request.uri
 #         pth = pth.replace("/", "")
 #         self.write(f"Hi Buddy, I am PartialViewHandler with {r} url {pth}")
-
-
-# class HoldingHandler(BaseAction):
-#     def get(self):
-#         # data = sd.get_holding_data()
-#         data = hld.my_report()
-#         r = data.to_json(orient='records')
-#         r = json.loads(r)
-#         self.finish({'data': r})
